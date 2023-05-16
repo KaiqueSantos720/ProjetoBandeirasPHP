473a628987c30130194e4c6c69c21dafe2f6aae6
nickolastg <nickmim969@gmail.com>
2023-05-15 23:51:12 -0300

    add full front-end first page

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $db = new PDO('mysql:host=localhost;dbname=projeto_paises_php;charset=utf8', 'root', 'testes-bancos-de-dados');
+        $db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=projeto_paises_php;charset=utf8', 'root', '123456');
     } catch (PDOException $e) {
         echo "<p>" . 'Erro: ' . $e->getMessage() . "</p>";
     }
--- index.php
+++ index.php
@@ -5,80 +5,112 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Países</title>
-    <link rel="stylesheet" href="css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="css/index.css" rel="stylesheet">
 </head>
 <body class="bg-dark text-light">
-    <header>
-        <form action="">
-            <input  name="q" type="text" id="q" placeholder="Pesquisar país">
-            <input type="submit" class="btn" name='pesquisar' value="Pesquisar">
-            <input type="submit" class="btn" name='reset' value="Cancelar">
-        </form>
+    <header class="bg-dark shadow">
+        <div class="container d-flex justify-content-between align-items-center py-2">
+            <a href="index.php" class="me-3">
+                <img src="https://via.placeholder.com/75x75?text=Logo" alt="Logo" class="logo">
+            </a>
+            <form action="" class="form-inline my-2 my-lg-0">
+                <div class="input-group mx-auto">
+                    <input name="q" type="text" id="q" placeholder="Pesquisar país" class="form-control mx-2">
+                    <button type="submit" class="btn btn-primary">Pesquisar</button>
+                    <button type="reset" class="btn btn-secondary bg-danger mx-2">Cancelar</button>
+                </div>
+            </form>
+        </div>
     </header>
-    <div>
-        <h1>Lista de Países</h1>
-        <?php
-            class Exibir
-            {
-                private $db;
-            
-                public function __construct()
+    <div class="container py-5">
+        <h1 id="text-principal">Lista de Países</h1>
+        <div class="row col-12 justify-content-start">
+            <?php
+                class Exibir
                 {
-                    $this->db = include('conexao.php');
-                }
-            
-                public function listarPaises($filtro = '')
-                {
-                    try {
-                        $sql = 'SELECT * FROM paises';
-                        if (!empty($filtro)) {
-                            $filtro = $this->db->quote("%$filtro%");
-                            $sql .= " WHERE Nome LIKE $filtro";
+                    private $db;
+        
+                    public function __construct()
+                    {
+                        $this->db = include('conexao.php');
+                    }
+        
+                    public function listarPaises($filtro = '')
+                    {
+                        try {
+                            $sql = 'SELECT * FROM paises';
+                            if (!empty($filtro)) {
+                                $filtro = $this->db->quote("%$filtro%");
+                                $sql .= " WHERE Nome LIKE $filtro";
+                            }
+                            $sql .= ' ORDER BY Nome ASC';
+        
+                            $consulta = $this->db->query($sql);
+                            $paises = $consulta->fetchAll();
+        
+                            return $paises;
+                        } catch (\Throwable $th) {
+                            echo $th;
                         }
-                        $sql .= ' ORDER BY Nome ASC';
-            
-                        $consulta = $this->db->query($sql);
-                        $paises = $consulta->fetchAll();
-            
-                        return $paises;
-                    } catch (\Throwable $th) {
-                        echo $th;
+        
+                        return [];
                     }
-            
-                    return [];
-                }
-            
-                public function exibirPaises($paises)
-                {
-                    foreach ($paises as $pais) {
-                        echo "<div class='m-4 d-flex flex-column align-items-start'>";
-                            echo "<img class='w-25' src='" . $pais["UrlImagem"] . "' >";
-                            echo "<a class='btn' href='show.php?" . http_build_query($pais) . "'>" . $pais["Nome"] . "</a>";
-                        echo "</div>";
+        
+                    public function exibirPaises($paises)
+                    {
+                        foreach ($paises as $pais) {
+                            echo "<div class='col-md-3 mb-4'>";
+                            echo "<div class='card cards text-center'>";
+                            echo "<img class='card-img-top' src='" . $pais["UrlImagem"] . "' alt='Card image'>";
+                            echo "<div class='card-body text-light'>";
+                            echo "<h5 class='card-title'>" . $pais["Nome"] . "</h5>";
+                            echo "<a href='show.php?" . http_build_query($pais) . "' class='btn btn-primary'>Ver mais</a>";
+                            echo "</div>";
+                            echo "</div>";
+                            echo "</div>";
+                        }
                     }
                 }
-            }
-            
-            $exibir = new Exibir();
-            
-            if (!isset($_GET['q']) || isset($_GET['reset'])) {
-                $paises = $exibir->listarPaises();
-            } else {
-                $pesquisa = trim($_GET['q']);
-                if ($pesquisa === '') {
-                    echo "Digite algo na barra de pesquisa";
-                    $paises = [];
+        
+                $exibir = new Exibir();
+        
+                if (!isset($_GET['q']) || isset($_GET['reset'])) {
+                    $paises = $exibir->listarPaises();
                 } else {
-                    $paises = $exibir->listarPaises($pesquisa);
-                    if (count($paises) === 0) {
-                        echo "<p>Nenhum resultado encontrado</p>";
+                    $pesquisa = trim($_GET['q']);
+                    if ($pesquisa === '') {
+                        echo "Digite algo na barra de pesquisa";
+                        $paises = [];
+                    } else {
+                        $paises = $exibir->listarPaises($pesquisa);
+                        if (count($paises) === 0) {
+                            echo "<p>Nenhum resultado encontrado</p>";
+                        }
                     }
                 }
-            }
-            
-            $exibir->exibirPaises($paises);
-        ?>
+        
+                $exibir->exibirPaises($paises);
+            ?>
+        </div>
     </div>
+    
+    <button onclick="topFunction()" id="myBtn" class="bg-danger" title="Go to top">Subir</button> 
+    
+    <footer class="shadow-top text-light text-center bg-dark py-2">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="py-2 me-3">
+                &copy;2023 Bandeiras - Todos os direitos reservados
+            </div>
+            <div>
+                <a href="index.php" class="me-3">
+                    <img src="https://via.placeholder.com/75x75?text=Logo" alt="Logo">
+                </a>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
