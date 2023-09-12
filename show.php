168f5fbd33351373254146473c3551343cbf6451
Kaique Santos <kaique.santos20042018@gmail.com>
2023-09-12 14:47:24 -0300

    interface da pagina show finalizada

--- show.php
+++ show.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="css/index.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title><?php echo $_GET['Nome'] ?></title>
 </head>
 <body class="bg-dark text-light">
@@ -19,11 +20,12 @@
                 <div class="input-group mx-auto">
                     <input name="q" type="text" id="q" placeholder="Pesquisar país" class="form-control mx-2">
                     <button type="submit" class="btn btn-primary">Pesquisar</button>
-                    <a href="index.php" class="btn btn-secondary bg-danger mx-2">Cancelar</a>
+                    <a href="index.php" class="btn btn-secondary bg-danger mx-2">Voltar</a>
                 </div>
             </form>
         </div>
     </header>
+
     <?php
         function filterInput($input) {
             return htmlspecialchars(strip_tags(trim($input)));
@@ -37,24 +39,36 @@
         $populacao = filterInput($_GET['Populacao']);
         $continente = filterInput($_GET['Continente']);
         $idh = filterInput($_GET['Idh']);
-
-        echo "<div class='m-4 d-flex flex-column align-items-start'>";
-            echo "<img class='w-25' src='{$urlImagem}' >";
-            echo "<p>{$nome}</p>";
-            echo "<p>{$capital}</p>";
-            echo "<p>{$lingua}</p>";
-            echo "<p>{$moeda}</p>";
-            echo "<p>{$populacao}</p>";
-            echo "<p>{$continente}</p>";
-            echo "<p>{$idh}</p>";
-        echo "</div>";
     ?>
 
-    <div onclick="topFunction()" id="myBtn" class="container bg-danger" title="Go to top">
-        <i class="bi bi-arrow-up-circle-fill"></i>
-    </div> 
+    <div class="container mt-5 mb-5">
+        <div class="card bg-dark text-light d-flex justify-content-center align-items-center border-0">
+            <div class="row no-gutters d-flex flex-column align-items-center p-3 border border-white rounded-3">
+                <div class="col-md-4 d-flex align-items-center p-2 gap-3 flex-column">
+                    <img src="<?php echo $urlImagem; ?>" class="card-img" alt="<?php echo $nome; ?>">
+                    <h5 class="card-title"><?php echo $nome; ?></h5>
+                </div>
+                <div class="col-md-8 d-flex">
+                    <div class="card-body">
+                        <p class="card-text"><strong>Capital:</strong> <?php echo $capital; ?></p>
+                        <p class="card-text"><strong>Lingua:</strong> <?php echo $lingua; ?></p>
+                        <p class="card-text"><strong>Moeda:</strong> <?php echo $moeda; ?></p>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text"><strong>População:</strong> <?php echo $populacao; ?></p>
+                        <p class="card-text"><strong>Continente:</strong> <?php echo $continente; ?></p>
+                        <p class="card-text"><strong>IDH:</strong> <?php echo $idh; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <footer class="shadow-top text-light text-center bg-dark py-2">
+    <div onclick="topFunction()" id="myBtn" class="container text-center" title="Go to top" style="cursor: pointer;">
+        <i class="fas fa-arrow-up"></i>
+    </div>
+
+    <footer class="shadow-top text-light text-center bg-dark ">
         <div class="content container d-flex justify-content-between align-items-center">
             <div class="py-2 me-3">
                 &copy;2023 Bandeiras - Todos os direitos reservados
